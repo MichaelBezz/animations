e097400507e43eb9a09dd12f861e942e2abd9ef3
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-05-18 17:08:14 +0300

    feat: добавит логику расчета header top

---
+++ source/js/modules/header/header-height.js
@@ -0,0 +1,10 @@
+export const setHeaderHeight = function () {
+  const header = document.querySelector('.header');
+
+  if (!header) {
+    return;
+  }
+
+  const headerRect = header.getBoundingClientRect();
+  document.documentElement.style.setProperty('--header-height', headerRect.height + 'px');
+};
