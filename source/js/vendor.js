bb68fda262fffb54c1ef8e3ed15ed04515d6d768
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-05-20 11:27:36 +0300

    feat:  добавит gsap

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,7 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+import {gsap} from './vendor/gsap.min.js';
+import {ScrollTrigger} from './vendor/scroll-trigger.min.js';
+
+gsap.registerPlugin(ScrollTrigger);
