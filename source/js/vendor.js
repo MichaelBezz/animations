2708e8bf61b795f789c1fb8889b660adaf577d5c
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-05-22 22:50:29 +0300

    feat: добавит splitting

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,7 +1,12 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+import Splitting from './vendor/splitting.min.js';
 import {gsap} from './vendor/gsap/gsap.min.js';
 import {ScrollTrigger} from './vendor/gsap/scroll-trigger.min.js';
 
 gsap.registerPlugin(ScrollTrigger);
+
+const splitting = new Splitting({
+  by: 'lines',
+});
