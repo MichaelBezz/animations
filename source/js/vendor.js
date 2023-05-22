cf1094a53e5f560f81c4a7fc89b5d0069c9fcd85
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-05-22 15:19:36 +0300

    fix(gsap): переместит файлы в одну папку

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,7 +1,7 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
-import {gsap} from './vendor/gsap.min.js';
-import {ScrollTrigger} from './vendor/scroll-trigger.min.js';
+import {gsap} from './vendor/gsap/gsap.min.js';
+import {ScrollTrigger} from './vendor/gsap/scroll-trigger.min.js';
 
 gsap.registerPlugin(ScrollTrigger);
