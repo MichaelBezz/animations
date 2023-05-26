2b22894e7cf48deee62e55876e346fe2620808e6
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-05-26 17:26:09 +0300

    fix: мелкие правки и рефакторинг

--- source/js/modules/header/toggle-menu.js
+++ source/js/modules/header/toggle-menu.js
@@ -1,20 +1,25 @@
 import {clickObserver} from '../../utils/observers';
+import {gsap} from '../../vendor/gsap/gsap.min.js';
 
 export const initToggleMenu = () => {
   const header = document.querySelector('.header');
   const menuToggle = document.querySelector('[data-menu="toggle"]');
   const menuNavigation = document.querySelector('[data-menu="navigation"]');
+  const menuLink = document.querySelectorAll('[data-menu="link"]');
 
-  if (!header || !menuToggle || !menuNavigation) {
+  if (!header || !menuToggle || !menuNavigation || !menuLink) {
     return;
   }
 
   let isMenuOpen = false;
+  const tweenOpen = gsap.from(menuLink, {yPercent: 110, delay: 0.4, stagger: 0.08});
+  const tweenClose = gsap.to(menuLink, {yPercent: 110, delay: 0.4, stagger: 0, paused: true});
 
   const closeMenu = () => {
     header.classList.remove('is-active');
     menuToggle.classList.remove('is-active');
     menuNavigation.classList.remove('is-active');
+    tweenClose.restart();
 
     window.scrollLock.enableScrolling();
     isMenuOpen = false;
@@ -24,6 +29,7 @@ export const initToggleMenu = () => {
     header.classList.add('is-active');
     menuToggle.classList.add('is-active');
     menuNavigation.classList.add('is-active');
+    tweenOpen.restart();
 
     window.scrollLock.disableScrolling();
     isMenuOpen = true;
